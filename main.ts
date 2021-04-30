bdb9747ae2c4c74d84d24e23d62bf43b54f1382b
Mafe2004 <82876378+Mafe2004@users.noreply.github.com>
2021-04-30 20:51:16 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 basic.showLeds(`
-    # . # # #
-    # . # . .
-    # # # # #
-    . . # . #
-    # # # . #
+    # . . . #
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
     `)
 let lluminositat = 39
 led.setBrightness(lluminositat)
 serial.writeLine("749494")
-basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P0)) {
-        lluminositat += 1
-        led.setBrightness(lluminositat)
-        basic.pause(100)
-        serial.writeLine("")
-    }
-})
 basic.forever(function () {
     if (true) {
         lluminositat += -1
         led.setBrightness(lluminositat)
         basic.pause(100)
         serial.writeLine("" + (lluminositat))
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        lluminositat += 1
+        led.setBrightness(lluminositat)
+        basic.pause(100)
+        serial.writeLine("")
     }
 })
